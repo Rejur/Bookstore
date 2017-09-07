2287d9d863c6d09fbe59f6ede42244f1d9118823
Rejur <wwwfeng321@gmail.com>
2017-09-07 20:32:04 +0800

    准备实体

---
+++ src/src/main/Java/com/book/pojo/Orderbook.java
@@ -0,0 +1,55 @@
+package com.book.pojo;
+
+import java.math.BigDecimal;
+
+public class Orderbook {
+    private Integer id;
+
+    private Integer orderid;
+
+    private Integer bookid;
+
+    private Integer quantity;
+
+    private BigDecimal unitprice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Integer orderid) {
+        this.orderid = orderid;
+    }
+
+    public Integer getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(Integer bookid) {
+        this.bookid = bookid;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitprice() {
+        return unitprice;
+    }
+
+    public void setUnitprice(BigDecimal unitprice) {
+        this.unitprice = unitprice;
+    }
+}
